a07df8445f068ae7d0674698e4b61089ada9c947
lauma4251 <starterlauma19@gmail.com>
2026-03-14 16:48:30 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './contexts/AuthContext.tsx';
-import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
         <App />
-        <Analytics />
       </AuthProvider>
     </HelmetProvider>
   </StrictMode>
