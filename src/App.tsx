0991bb3a8c93988609d2733741d05cff9c63028f
lauma4251 <starterlauma19@gmail.com>
2026-03-14 15:04:23 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import AppRoutes from "@/routes";
 
