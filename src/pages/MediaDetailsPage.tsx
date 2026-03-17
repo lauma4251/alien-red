50eddf39864df083e8b893de23228038e0026956
lauma4251 <starterlauma19@gmail.com>
2026-03-17 18:33:37 +0700

    Update MediaDetailsPage.tsx

--- src/pages/MediaDetailsPage.tsx
+++ src/pages/MediaDetailsPage.tsx
@@ -448,6 +448,16 @@ const MediaDetailsPage: React.FC<MediaDetailsPageProps> = () => {
         selectedEpisode?.episode_number || 1
       }`;
     }
+	
+    // CEK SERVER DISINI
+    if (["vidcore", "vidrock"].includes(selectedServer)) {
+      // Karena ini server 'clean' (gak ada ? sebelumnya), kita pakai ?
+      return `${finalUrl}?nextButton=false&autoNext=false`;
+    }
+
+    return finalUrl;
+	
+	
   };
 
   const getMediaTitle = () => {
